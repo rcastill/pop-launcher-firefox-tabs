18ceec211a892c0234b8cbcb7a7440dab6efff6b
Rodolfo <rodolfocastillomateluna@gmail.com>
2022-05-29 13:23:55 -0400

    List tabs poc

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use firefox_rs::{list_tabs, FFResult};
 use futures_lite::{AsyncWriteExt, StreamExt};
 use pop_launcher::{
     async_stdin, async_stdout, json_input_stream, PluginResponse, PluginSearchResult, Request,
 };
 use std::io::Stdout;
+
+macro_rules! trycont {
+    ($result:expr, $literal:expr) => {
+        match $result {
+            Ok(ok) => ok,
+            Err(e) => {
+                log::error!($literal, e);
+                continue;
+            }
+        }
+    };
+}
 
 struct Responder {
     out: blocking::Unblock<Stdout>,
@@ -29,16 +42,31 @@ impl Responder {
 struct Plugin {}
 
 impl Plugin {
-    async fn search(&mut self, _query: &str, responder: &mut Responder) {
-        let search = PluginSearchResult {
-            id: 0,
-            name: "huachimingo".into(),
-            description: "test".into(),
-            ..Default::default()
-        };
-        let res = PluginResponse::Append(search);
-        responder.send(res).await;
+    async fn search(&mut self, _query: &str, responder: &mut Responder) -> FFResult<()> {
+        let tabs = list_tabs()?;
+        let results = tabs
+            .into_iter()
+            .enumerate()
+            .map(|(i, tab)| PluginSearchResult {
+                id: i as u32,
+                name: tab.title,
+                description: "firefox tab".into(),
+                ..Default::default()
+            })
+            .map(PluginResponse::Append);
+        for search in results {
+            responder.send(search).await;
+        }
+        // let search = PluginSearchResult {
+        //     id: 0,
+        //     name: "huachimingo".into(),
+        //     description: "test".into(),
+        //     ..Default::default()
+        // };
+        // let res = PluginResponse::Append(search);
+        // responder.send(res).await;
         responder.send(PluginResponse::Finished).await;
+        Ok(())
     }
 }
 
@@ -50,14 +78,15 @@ async fn main() {
     let mut responder = Responder::default();
 
     // main loop
-    while let Some(req_res) = requests.next().await {
-        match req_res {
-            Ok(request) => match &request {
-                Request::Search(query) => plugin.search(query, &mut responder).await,
-                Request::Exit => break,
-                unsup => log::debug!("Unsupported request: {unsup:?}"),
-            },
-            Err(e) => log::error!("Failed to parse request: {e}"),
+    while let Some(request_res) = requests.next().await {
+        let request = trycont!(request_res, "Failed to parse request: {}");
+        match request {
+            Request::Search(query) => trycont!(
+                plugin.search(&query, &mut responder).await,
+                "Failed to search: {}"
+            ),
+            Request::Exit => break,
+            other => log::debug!("Unsupported request: {other:?}"),
         }
     }
 }
